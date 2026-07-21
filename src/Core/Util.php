e115860bd9fdfd8a8249ac2fee17509a912c4cb0
SothisKrieg <ftatilon@gmail.com>
2026-07-21 15:08:31 +0200

    seal

--- src/Core/Util.php
+++ src/Core/Util.php
@@ -211,7 +211,7 @@ final class Util
         parse_str($base->getQuery(), $q1);
         parse_str($parsed['query'] ?? '', $q2);
 
-        $mergedQuery = array_merge_recursive($q1, $q2, $query);
+        $mergedQuery = array_replace_recursive($q1, $q2, $query);
 
         /** @var array<string,mixed> */
         $normalizedQuery = self::mapRecursive(
