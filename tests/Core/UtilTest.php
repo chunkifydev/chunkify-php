6b89dcbb084361716e19b8f663d6c244cd2c7c85
SothisKrieg <ftatilon@gmail.com>
2026-07-21 11:34:52 +0200

    Build SDK
    Stainless-Generated-From: f2131bc64ccbd131a6e8766f4b2f08d037e4f85a

--- tests/Core/UtilTest.php
+++ tests/Core/UtilTest.php
@@ -48,6 +48,10 @@ class UtilTest extends TestCase
     {
         $factory = Psr17FactoryDiscovery::findUriFactory();
         $base = $factory->createUri('http://localhost');
+        $util = new \ReflectionClass(Util::class);
+        $arrayFormat = $util->getConstant('QUERY_ARRAY_FORMAT');
+        $nestedFormat = $util->getConstant('QUERY_NESTED_FORMAT');
+        $nestedKey = 'dots' === $nestedFormat ? 'dog.dog' : 'dog%5Bdog%5D';
         $cases = [
             [
                 '',
@@ -67,17 +71,40 @@ class UtilTest extends TestCase
             [
                 '',
                 ['dog' => ['dog']],
-                'http://localhost?dog[0]=dog',
+                match ($arrayFormat) {
+                    'brackets' => 'http://localhost?dog%5B%5D=dog',
+                    'indices' => 'http://localhost?dog%5B0%5D=dog',
+                    'comma', 'repeat' => 'http://localhost?dog=dog',
+                },
             ],
             [
                 '',
                 ['dog' => [true, false]],
-                'http://localhost?dog[0]=true&dog[1]=false',
+                match ($arrayFormat) {
+                    'brackets' => 'http://localhost?dog%5B%5D=true&dog%5B%5D=false',
+                    'indices' => 'http://localhost?dog%5B0%5D=true&dog%5B1%5D=false',
+                    'comma' => 'http://localhost?dog=true%2Cfalse',
+                    'repeat' => 'http://localhost?dog=true&dog=false',
+                },
             ],
             [
                 '',
                 ['dog' => ['dog' => ['dog']]],
-                'http://localhost?dog[dog][0]=dog',
+                match ($arrayFormat) {
+                    'brackets' => "http://localhost?{$nestedKey}%5B%5D=dog",
+                    'indices' => "http://localhost?{$nestedKey}%5B0%5D=dog",
+                    'comma', 'repeat' => "http://localhost?{$nestedKey}=dog",
+                },
+            ],
+            [
+                '',
+                ['metadata' => [['group:test_jobs'], ['uploaded_by:user_demo']]],
+                match ($arrayFormat) {
+                    'brackets' => 'http://localhost?metadata%5B%5D%5B%5D=group%3Atest_jobs&metadata%5B%5D%5B%5D=uploaded_by%3Auser_demo',
+                    'indices' => 'http://localhost?metadata%5B0%5D%5B0%5D=group%3Atest_jobs&metadata%5B1%5D%5B0%5D=uploaded_by%3Auser_demo',
+                    'comma' => 'http://localhost?metadata=group%3Atest_jobs%2Cuploaded_by%3Auser_demo',
+                    'repeat' => 'http://localhost?metadata=group%3Atest_jobs&metadata=uploaded_by%3Auser_demo',
+                },
             ],
         ];
 
